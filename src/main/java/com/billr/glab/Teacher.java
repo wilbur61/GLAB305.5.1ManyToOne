e29180059b3fb58cae8c0a4329a6153473eb791f
wilbur61 <wroales@perscholas.org>
2023-08-25 20:29:44 -0400

    Added my project

---
+++ src/main/java/com/billr/glab/Teacher.java
@@ -0,0 +1,63 @@
+package com.billr.glab;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table
+public class Teacher implements Serializable  {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue( strategy=GenerationType.IDENTITY )
+	private int tit;
+	private String salary;
+	private String Teachername;
+
+	//Let us consider an example of a relationship between Teacher and a 
+	//department entity. A Department consists of multiple Teachers, but 
+	//each Teacher belongs to only one Department. That is a typical example 
+	//for a many-to-one relationship or association. We will model this in a 
+	//database that we need to store the primary key of the Department record 
+	//in as a foreign key in the Teacher table.
+	//
+	//
+	
+	@ManyToOne
+	private Department department;
+	public Teacher(int tit, String salary, String teachername) {
+		super();
+		this.tit = tit;
+		this.salary = salary;
+		Teachername = teachername;
+	}
+	public Teacher()
+	{}
+	
+	public Department getDep() {
+		return department;
+	}
+	public void setDep(Department department) {
+		this.department = department;
+	}
+	public int getTit() {
+		return tit;
+	}
+	public void setTit(int tit) {
+		this.tit = tit;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getTeachername() {
+		return Teachername;
+	}
+	public void setTeachername(String teachername) {
+		Teachername = teachername;
+	}	
+}
